d5352f162cf4262068b33ae21e487a3964e3f1cd
“SahilKharya” <kharyasahil@gmail.com>
2020-01-21 22:58:00 +0530

    few changes

--- NameCorrect.java
+++ NameCorrect.java
@@ -2,42 +2,30 @@ import java.util.Scanner;
 
 public class NameCorrect {
     public static void main(String[] args) {
-        try{
-        int vowelCount = 0;
-        int consonantCount = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string : ");
-        String input = sc.nextLine();
-        System.out.println("\nCorrect name would be :");
+        try {
+            int vowelCount = 0;
+            int consonantCount = 0;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the string : ");
+            String input = sc.nextLine();
+            System.out.println("\nCorrect name would be :");
 
-        String[] name = input.split(" ");
-        for(int i=0;i<=name.length-1;i++) {
-            if(i!= name.length-1 ) {
-                name[i] = name[i].toUpperCase();
-                //System.out.println(name[i]);
-
-                char firstLetter = name[i].charAt(0);
-                System.out.print(firstLetter + ". ");
+            String[] name = input.split(" ");
+            for (int i = 0; i <= name.length - 1; i++) {
+                if (i != name.length - 1) {
+                    name[i] = name[i].toUpperCase();
+                    char firstLetter = name[i].charAt(0);
+                    System.out.print(firstLetter + ". ");
+                } else {
+                    name[i] = name[i].toLowerCase();
+                    char[] lastname = name[i].toCharArray();
+                    lastname[0] = Character.toUpperCase(lastname[0]);
+                    System.out.println(lastname);
+                }
             }
-            else{
-                //System.out.println("last :" +name[i] );
-                name[i]=name[i].toLowerCase();
-                //System.out.println("last :" +name[i] );
-
-                char[] lastname = name[i].toCharArray();
-                lastname[0]=Character.toUpperCase(lastname[0]);
-                lastname.toString();
-                System.out.println(lastname);
-                //name[i].toUpperCase(name.charAt(0));
-            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        //String lastName = name[name.length-1];
-       //     lastName = lastName.toLowerCase();
-         //   lastName = lastName.toUpperCase(.charAt(0));
-        //System.out.print(lastName);
-
-    } catch(Exception e){
-        System.out.println(e);
-    }}
+    }
 
 }
