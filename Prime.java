f336e7851a750f232f454c5aca2c7f3da97a5bd7
“SahilKharya” <kharyasahil@gmail.com>
2020-01-24 17:04:20 +0530

    changes

--- Prime.java
+++ Prime.java
@@ -8,20 +8,23 @@ public class Prime {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter a number: ");
             int number = sc.nextInt();
-
+            int temp = 0;
             if (number == 1)
                 System.out.println("1 is not a prime number");
-            for (int i = 2; i < number / 2; i++) {
+            for (int i = 2; i <= number / 2; i++) {
                 if (number % i == 0) {
                     System.out.println("Number is not a prime number");
-                } else {
-                    System.out.println("Number is a prime number");
+                    temp = 1;
+                    break;
                 }
-                break;
-
             }
-        } catch (Exception e) {
-            System.out.println(e);
+            if (temp == 0) {
+                System.out.println("Number is a prime number");
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
+
