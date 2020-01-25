72312a454baf8eeff21406176ae6128b8a908aba
“SahilKharya” <kharyasahil@gmail.com>
2020-01-25 19:07:10 +0530

    Updated

--- EmptySpaces.java
+++ EmptySpaces.java
@@ -2,24 +2,23 @@
 
 public class EmptySpaces {
 	public static void main(String[] args) {
-		String text = "This is a Java Program to remove all white spaces from a String.";
+		String text = "This  is Java   Program to     Remove blank spaces.    ";
 		System.out.print(text);
-		char[] c = removeSpaces(text);
-		String noSpacesText = new String(c);
+		String noSpacesText = removeSpaces(text);;
 		System.out.print("\n\n" + noSpacesText);
 	}
 
 	// Method to make a Array without any blank spaces characters in it.
-	public static char[] removeSpaces(String line) {
+	public static String removeSpaces(String line) {
 		char[] charArray = line.toCharArray();
-		char[] newArray = new char[charArray.length];
+		String newArray = "";
 		for (int i = 0; i < charArray.length; i++) {
 			if (charArray[i] == " ".charAt(0)) {
-				charArray[i] = charArray[i + 1];
 				i++;
 			}
-			// System.out.print(charArray[i] + ",");
-			newArray[i] = charArray[i];
+			else{
+				newArray = newArray + charArray[i];
+			}
 		}
 		return newArray;
 	}
