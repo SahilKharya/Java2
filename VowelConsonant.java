c59dfb392291ce9cf29d4daad09aedb7077bd618
“SahilKharya” <kharyasahil@gmail.com>
2020-01-28 13:55:05 +0530

    few corrections

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -10,20 +10,23 @@ public class VowelConsonant {
         System.out.println("Enter the string : ");
         String input = sc.nextLine();
         input = input.toLowerCase();
-        char[] stringArray = input.toCharArray();
         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        for (int j = 0; j < stringArray.length; j++) {
-            for (int i = 0; i < vowels.length; i++) {
-                if (stringArray[j] == vowels[i]) {
-                    vowelCount++;
-                    break;
 
-                } else if (stringArray[j] >= 'a' && stringArray[j] <= 'z') {
-                    consonantCount++;
-                    break;
+        for (int j = 0; j < input.length(); j++) {
+            for (int i = 0; i < vowels.length; i++) {
+
+                if (input.charAt(j) == vowels[i]) {
+                    vowelCount++;
                 }
+
             }
+            if (input.charAt(j) >= 'a' && input.charAt(j) <= 'z') {
+                consonantCount++;
+            }
+
+
         }
+        consonantCount = consonantCount-vowelCount;
         System.out.println("No. of Vowels : " + vowelCount);
         System.out.println("No. of Consonants : " + consonantCount);
     }
